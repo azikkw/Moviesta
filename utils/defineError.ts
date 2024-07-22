9e6095825180b16d0a1866857113e091b522d86c
Azat Amen <94234353+azikkw@users.noreply.github.com>
2024-07-22 23:23:06 +0500

    Update defineError.ts

--- utils/defineError.ts
+++ utils/defineError.ts
@@ -2,7 +2,7 @@ export default function (error: any) {
     if(error === 'auth/email-already-in-use') {
         return 'This email is already in use.';
     } else if(error === 'auth/weak-password') {
-        return 'The password is short.';
+        return 'Password should be at least 6 characters.';
     } else if(error === 'auth/invalid-credential') {
         return 'Wrong email or password.'
     } else if(error === 'not image') {
